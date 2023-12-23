3ad386a292086f88b00fa97e73fd1684e2b36a2c
Adarsh <adarshofficial.1607@gmail.com>
2023-12-23 22:16:24 +0530

    styling pop up

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const button = document.querySelector('button')
-const inputText = document.querySelector('.text')
-const reward = document.querySelector('.reward')
+const button = document.querySelector("button");
+const inputText = document.querySelector(".text");
+const reward = document.querySelector(".reward");
 
-button.addEventListener('click', (e) => {
-  e.preventDefault()
-  inputValue = inputText.value
-  inputText.value = ''
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputValue = inputText.value;
+  inputText.value = "";
   sum = 0;
-  for(let i = 0; i < inputValue.length; i++) {
-    sum += parseInt(inputValue[i])
+  for (let i = 0; i < inputValue.length; i++) {
+    sum += parseInt(inputValue[i]);
   }
 
-  if(inputValue === ''){
-    Swal.fire("Please type anything");
-  }else if(inputValue.length === 7){
-    Swal.fire("Thala For A Reason");
-  }else if(sum === 7){
-    Swal.fire("Thala For A Reason");
-  }else{
-    Swal.fire("No Thala For You");
+  if (inputValue === "") {
+    Swal.fire({
+      title: "Please type anything",
+      padding: "10px, 0 20px, 0",
+    });
+  } else if (inputValue.length === 7) {
+    Swal.fire({
+      title: "Thala For A Reason",
+      padding: "10px, 0 20px, 0",
+    });
+  } else if (sum === 7) {
+    Swal.fire({
+      title: "Thala For A Reason",
+      padding: '10px, 0 20px, 0'
+    });
+  } else {
+    Swal.fire({
+      title: "No Thala For You",
+      padding: '10px, 0 20px, 0'
+    });
   }
-})
+});
