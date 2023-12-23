c2dfa7b563e559c4857d65d55d7a22fc404a8350
Adarsh <adarshofficial.1607@gmail.com>
2023-12-23 23:19:45 +0530

    adding images

--- script.js
+++ script.js
@@ -13,18 +13,22 @@ button.addEventListener("click", (e) => {
 
   if (inputValue === "") {
     Swal.fire({
-      title: "Please type anything",
+      title: "Please type something",
       padding: "10px, 0 20px, 0",
     });
   } else if (inputValue.length === 7) {
     Swal.fire({
       title: "Thala For A Reason",
       padding: "10px, 0 20px, 0",
+      imageUrl: 'https://imgs.search.brave.com/yUFRDhCJud3iilfFOTOdp4UOkta4ymIdFiTHMnr5Rw0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9vZmZp/Y2lhbHNwb3J0c2h1/Yi5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMjMvMDYvVGhh/bGEtbWluLTEwMjR4/NjQwLmpwZw',
+      imageAlt: 'Thala Image'
     });
   } else if (sum === 7) {
     Swal.fire({
       title: "Thala For A Reason",
-      padding: '10px, 0 20px, 0'
+      padding: '10px, 0 20px, 0',
+      imageUrl: 'https://imgs.search.brave.com/yUFRDhCJud3iilfFOTOdp4UOkta4ymIdFiTHMnr5Rw0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9vZmZp/Y2lhbHNwb3J0c2h1/Yi5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMjMvMDYvVGhh/bGEtbWluLTEwMjR4/NjQwLmpwZw',
+      imageAlt: 'Thala Image'
     });
   } else {
     Swal.fire({
